b6803d51425395f7d6bcc6e3046823e181613596
Darren Morman <darrenmorman@gmail.com>
2021-07-06 11:50:41 +0100

    Upgrade to bootstrap 5

--- dist/jqueryui-editable/js/jqueryui-editable.js
+++ dist/jqueryui-editable/js/jqueryui-editable.js
@@ -1,7 +1,7 @@
 /*! X-editable - v1.5.3 
 * In-place editing with Twitter Bootstrap, jQuery UI or pure jQuery
-* http://github.com/vitalets/x-editable
-* Copyright (c) 2018 Vitaliy Potapov; Licensed MIT */
+* http://github.com/mormanski/x-editable
+* Copyright (c) 2021 Vitaliy Potapov; Licensed MIT */
 /**
 Form with single input element, two buttons and two states: normal/loading.
 Applied as jQuery method to DIV tag (not to form tag!). This is because form can be in loading state when spinner shown.
